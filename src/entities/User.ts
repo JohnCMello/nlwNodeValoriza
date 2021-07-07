3fd3582da4999d19cc7787f43a5b18749c4d3f36
JohnCMello <johncharlesmello@gmail.com>
2021-07-07 16:13:12 -0300

    add migration and entity folders and files

---
+++ src/entities/User.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+import { v4 } from "uuid"
+
+@Entity()
+class User {
+
+  @PrimaryColumn()
+  readonly id: string;
+  @Column()
+  name: string;
+  @Column()
+  email: string;
+  @Column()
+  admin: boolean;
+  @CreateDateColumn()
+  created_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
+
+}
+
+export { User }
