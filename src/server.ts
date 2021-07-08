ef10a52c6d3014c31eb80e46bce3cea940b408b7
JohnCMello <johncharlesmello@gmail.com>
2021-07-08 17:15:08 -0300

    add router to server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import "reflect-metadata";
 import express from "express";
 
+import { router } from "./routes";
+
 import "./database";
 
 const app = express();
 
-app.listen(3000, () => console.log("Hello Express"))
+app.use(router);
+
+app.listen(3000, () => console.log("Hello Express"));
