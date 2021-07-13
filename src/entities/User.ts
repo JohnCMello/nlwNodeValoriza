60d918dccef13b7dbaf29a87730b3121c2cef570
JohnCMello <johncharlesmello@gmail.com>
2021-07-13 17:28:52 -0300

    add entity: User

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -7,21 +7,27 @@ class User {
 
   @PrimaryColumn()
   readonly id: string;
+
   @Column()
   name: string;
+
   @Column()
   email: string;
+
   @Column()
   admin: boolean;
+
+  @Column()
+  password: string
+
   @CreateDateColumn()
   created_at: Date;
+
   @UpdateDateColumn()
   updated_at: Date;
 
   constructor() {
-    if (!this.id) {
-      this.id = uuid()
-    }
+    if (!this.id) this.id = uuid()
   }
 
 }
