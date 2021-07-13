6acaf20641a7d792912ecea87ad19bf0f26fbd28
JohnCMello <johncharlesmello@gmail.com>
2021-07-13 17:16:17 -0300

    add routes: tags, auth, compliments

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController"
+import { CreateTagController } from "./controllers/CreateTagController"
+import { CreateComplimentController } from "./controllers/CreateComplimentController"
+import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 const router = Router();
 
 const createUserController = new CreateUserController()
+const authenticateUserContoller = new AuthenticateUserController()
+const createTagController = new CreateTagController()
+const createComplimentController = new CreateComplimentController()
 
+
+router.post("/tags", ensureAdmin, createTagController.handle)
 router.post("/users", createUserController.handle)
+router.post("/auth,", authenticateUserContoller.handle)
+router.post("/compliments", createComplimentController.handle)
 
 export { router }
