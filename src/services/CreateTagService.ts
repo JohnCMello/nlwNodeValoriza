fef4b728219bf58bb512509c4d1ad3e5cd7a74ba
JohnCMello <johncharlesmello@gmail.com>
2021-07-13 17:30:01 -0300

    create service: Create Tag

--- src/services/CreateTagService.ts
+++ src/services/CreateTagService.ts
@@ -1,26 +1,22 @@
-import { getCustomRepository } from "typeorm"
+import { getCustomRepository } from "typeorm";
 import { TagsRepos } from "../repos/TagsRepos";
 
 class CreateTagService {
 
   async execute(name: string) {
-    const tagsRepository = getCustomRepository(TagsRepos);
+    const tagsRepos = getCustomRepository(TagsRepos);
 
-    if (!name) {
-      throw new Error("Name not provided. Please, provide a tag name.")
-    }
+    if (!name) throw new Error("Name not provided. Please, provide a tag name.");
 
-    const tagAlreadyExists = await tagsRepository.findOne({ name });
+    const tagAlreadyExists = await tagsRepos.findOne({ name });
 
-    if (tagAlreadyExists) {
-      throw new Error("There is already a tag registered under this name")
-    }
+    if (tagAlreadyExists) throw new Error("There is already a tag registered under this name");
 
-    const tag = tagsRepository.create({ name })
+    const tag = tagsRepos.create({ name });
 
-    await tagsRepository.save(tag)
+    await tagsRepos.save(tag);
 
-    return tag
+    return tag;
   }
 }
 
