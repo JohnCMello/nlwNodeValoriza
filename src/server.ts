89cc4ba5bfd1922dfef276e15cb73737bb752574
JohnCMello <johncharlesmello@gmail.com>
2021-07-13 17:15:04 -0300

    add express async error

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import express, { Request, Response, NextFunction, response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors"
 
 import { router } from "./routes";
@@ -14,10 +14,10 @@ app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
-    return response.status(400).json({ error: err.message })
+    return res.status(400).json({ error: err.message })
   }
 
-  return response.status(500).json({
+  return res.status(500).json({
     status: "error",
     message: "Internal Server Error"
   })
