02b6888c9e1dfaf2733d04182aed0203e5c5cff8
JohnCMello <johncharlesmello@gmail.com>
2021-07-13 17:25:12 -0300

    add controller: Create Tag

--- src/controllers/CreateTagController.ts
+++ src/controllers/CreateTagController.ts
@@ -2,13 +2,13 @@ import { Request, Response } from "express";
 import { CreateTagService } from "../services/CreateTagService";
 
 class CreateTagController {
-  async handle(request: Request, response: Response) {
-    const { name } = request.body;
+  async handle(req: Request, res: Response) {
+    const { name } = req.body;
 
     const createTagService = new CreateTagService();
     const tag = await createTagService.execute(name);
 
-    return response.json(tag)
+    return res.json(tag)
   }
 }
 
